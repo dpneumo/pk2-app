6831cacaabe31fb44fb63b32c02d20588f8ecf8a
Mitchell C Kuppinger <dpneumo@gmail.com>
2018-01-21 14:14:09 -0600

    remove echo pwd

--- scripts/ruby.sh
+++ scripts/ruby.sh
@@ -15,7 +15,6 @@ iptables -D wan_out -p tcp --dport 80  -m state --state NEW,ESTABLISHED -j ACCEP
 # rbenv
 sudo -u loco git clone https://github.com/rbenv/rbenv.git /home/loco/.rbenv
 sudo -u loco cd /home/loco/.rbenv
-sudo -u loco echo ${pwd}
 sudo -u loco src/configure && make -C src
 sudo -u loco echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> /home/loco/.bash_profile
 sudo -u loco source /home/loco/.bash_profile
