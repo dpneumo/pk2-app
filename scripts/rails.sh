a9b9d5e22272ca1d4aec35978e2bf4de1d0de2bf
Mitchell C Kuppinger <dpneumo@gmail.com>
2018-02-15 10:51:34 -0600

    Pull app on to the app server.

--- scripts/rails.sh
+++ scripts/rails.sh
@@ -1,14 +1,33 @@
+#!/bin/bash
+
+# Add HTTP & HTTPS Client rules - temporary for yum
+sudo sh -c "
+  iptables -A wan_in  -p tcp --sport 80  -m state --state ESTABLISHED     -j ACCEPT;
+  iptables -A wan_out -p tcp --dport 80  -m state --state NEW,ESTABLISHED -j ACCEPT;
+  iptables -A wan_in  -p tcp --sport 443 -m state --state ESTABLISHED     -j ACCEPT;
+  iptables -A wan_out -p tcp --dport 443 -m state --state NEW,ESTABLISHED -j ACCEPT;
+  /sbin/service iptables save;"
+
+# Development tools
 sudo yum groupinstall 'Development Tools'
-
-cd ~
-git clone https://github.com/dpneumo/pk2.git
-
-cd pk2
-bundle install
-
 
 # https://yarnpkg.com/en/docs/install (Centos7)
 sudo yum install -y gcc-c++ make
 sudo wget https://dl.yarnpkg.com/rpm/yarn.repo -O /etc/yum.repos.d/yarn.repo
 curl --silent --location https://rpm.nodesource.com/setup_6.x | sudo bash -
 sudo yum install -y yarn
+
+# Clone the app
+cd ~
+git clone https://github.com/dpneumo/pk2.git
+
+cd pk2
+bundle install
+
+# Remove HTTP & HTTPS Client rules
+sudo sh -c "
+  iptables -D wan_in  -p tcp --sport 80  -m state --state ESTABLISHED     -j ACCEPT;
+  iptables -D wan_out -p tcp --dport 80  -m state --state NEW,ESTABLISHED -j ACCEPT;
+  iptables -D wan_in  -p tcp --sport 443 -m state --state ESTABLISHED     -j ACCEPT;
+  iptables -D wan_out -p tcp --dport 443 -m state --state NEW,ESTABLISHED -j ACCEPT;
+  /sbin/service iptables save"
